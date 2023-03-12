b9c59181f7939a6bdf8ef4bcc1cb902ea2cf4cb1
gawao <gawao57@gmail.com>
2023-03-13 00:02:12 +0900

    Web APIの呼び出し結果画面表示

--- translate_app/README.md
+++ translate_app/README.md
@@ -1,4 +1,4 @@
 # translate_app
 
-[WEB+DB PRESS Vol.130](https://amzn.asia/d/dcAJ6Rn) 
+[WEB+DB PRESS Vol.130](https://gihyo.jp/magazine/wdpress/archive/2022/vol130) 
 特集2 いまはじめるFlutter　iOS/Android両対応アプリを開発してみよう
--- translate_app/lib/main.dart
+++ translate_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -48,73 +49,155 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _formKey = GlobalKey<FormState>();
-
-  // TextEditingController TextField Widgetの入力文字や選択文字を取得、変更する機能を持つ
-  final _textEditController = TextEditingController();
-
+  String? _result;
+  bool _isLoading = false;
 
   Future<void> _translate(String sentence) async {
-    final url = Uri.parse('https://labs.goo.ne.jp/api/hiragana');
-    final headers = {'Content-Type': 'application/json'};
-    final body = json.encode({
-      'app_id': '<token>',
-      'sentence': sentence,
-      'output_type': 'hiragana'
+    setState(() {
+      _isLoading = true;
     });
-    final response = await http.post(url, headers: headers, body: body);
-    final responseJson = json.decode(response.body) as Map<String, dynamic>;
-    debugPrint(responseJson['converted']);
+
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final headers = {'Content-type': 'application/json; charset=UTF-8'};
+
+    final rand = new math.Random();
+    final body = json.encode(
+        // {'app_id': '<token>', 'sentence': sentence, 'output_type': 'hiragana'});
+        {'title': rand.nextInt(10), 'body': sentence, 'userid': 1});
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+      final responseJson = json.decode(response.body) as Map<String, dynamic>;
+      debugPrint('$responseJson');
+
+      setState(() {
+        _result = responseJson['body'];
+        _isLoading = false;
+      });
+    } catch (e) {
+      // ignore: todo
+      // TODO: error
+      debugPrint('error!');
+      debugPrint(e.toString());
+    }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: const Text("Translate App")),
-        // 子Widgetを垂直に配置するWidget
-        body: Form(
-            key: _formKey,
-            child: Column(
-              // 子Widgetを中央へ寄せるためのパラメータ
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // 余白を与えて子要素を配置するWidget
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: TextFormField(
-                        controller: _textEditController,
-                        maxLength: 5,
-                        decoration:
-                            const InputDecoration(hintText: '文章を入力してください'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return '文章が入力されていません';
-                          }
-                          return null;
-                        })),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                    onPressed: () {
-                      final formState = _formKey.currentState!;
-                      if (!formState.validate()) {
-                        return;
-                      }
-
-                      debugPrint('text = ${_textEditController.text}');
-                    },
-                    child: const Text('変換'))
-              ],
-            )));
+      appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Text("Translate App")),
+      // 子Widgetを垂直に配置するWidget
+      body: _body(context),
+    );
   }
 
+  Widget _body(BuildContext context) {
+    final result = _result;
+    if (result != null) {
+      return _Result(
+          sentence: result,
+          onTapBack: () {
+            setState(() {
+              _result = null;
+            });
+          });
+    } else if (_isLoading) {
+      return const _Loading();
+    } else {
+      return _InputForm(onSubmit: _translate);
+    }
+  }
+  // @override
+  // void dispose() {
+  //   _textEditController.dispose();
+  //   super.dispose();
+  // }
+}
+
+class _InputForm extends StatefulWidget {
+  const _InputForm({super.key, required this.onSubmit});
+
+  final Function(String) onSubmit;
   @override
-  void dispose() {
-    _textEditController.dispose();
-    super.dispose();
+  State<StatefulWidget> createState() => _InputFormState();
+}
+
+class _InputFormState extends State<_InputForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _textEditController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          // 子Widgetを中央へ寄せるためのパラメータ
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // 余白を与えて子要素を配置するWidget
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                    controller: _textEditController,
+                    maxLength: 5,
+                    decoration: const InputDecoration(hintText: '文章を入力してください'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '文章が入力されていません';
+                      }
+                      return null;
+                    })),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  final formState = _formKey.currentState!;
+                  if (!formState.validate()) {
+                    return;
+                  }
+                  widget.onSubmit(_textEditController.text);
+                  debugPrint('text = ${_textEditController.text}');
+                },
+                child: const Text('変換'))
+          ],
+        ));
+  }
+}
+
+class _Loading extends StatelessWidget {
+  const _Loading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      // 回転アニメーションする円形のインジケータwidget
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class _Result extends StatelessWidget {
+  const _Result({super.key, required this.sentence, required this.onTapBack});
+
+  final String sentence;
+  final void Function() onTapBack;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(sentence)),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: onTapBack, child: const Text('再入力'))
+        ],
+      ),
+    );
   }
 }
